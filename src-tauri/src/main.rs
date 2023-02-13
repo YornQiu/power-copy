cd00315e3162c0b4948b59c129f46bd9ca19b11a
Yorn Qiu <yornqiu@gmail.com>
2023-02-13 17:30:18 +0800

    feat: window shadow

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,24 @@
+/*
+ * @Author: Yorn Qiu
+ * @Date: 2023-02-10 08:59:25
+ * @LastEditors: Yorn Qiu
+ * @LastEditTime: 2023-02-13 16:32:35
+ * @Description: file content
+ * @FilePath: /power-copy/src-tauri/src/main.rs
+ */
+
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
 
+use setup::set_window_shadow;
+
 mod setup;
 
 fn main() {
     tauri::Builder::default()
+        .setup(set_window_shadow)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/setup.rs
+++ src-tauri/src/setup.rs
@@ -1,7 +1,17 @@
+/*
+ * @Author: Yorn Qiu
+ * @Date: 2023-02-10 14:41:02
+ * @LastEditors: Yorn Qiu
+ * @LastEditTime: 2023-02-13 16:34:04
+ * @Description: file content
+ * @FilePath: /power-copy/src-tauri/src/setup.rs
+ */
+
 use tauri::{App, Manager};
 use window_vibrancy::{self, NSVisualEffectMaterial};
+use window_shadows::set_shadow;
 
-pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
+pub fn set_window_vibrancy(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
     let window = app.get_window("main").unwrap();
 
     #[cfg(target_os = "macos")]
@@ -11,6 +21,13 @@ pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>
     #[cfg(target_os = "windows")]
     window_vibrancy::apply_blur(&window, Some((18, 18, 18, 125)))
         .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+    Ok(())
+}
+
+pub fn set_window_shadow(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>> {
+    let window = app.get_window("main").unwrap();
+    set_shadow(&window, true).expect("Unsupported platform!");
 
     Ok(())
 }
