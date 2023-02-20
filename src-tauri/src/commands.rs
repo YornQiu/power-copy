2d6f2a32d39f25b0383fd80cfd40d52da5e7fdf8
Yorn Qiu <yornqiu@gmail.com>
2023-02-20 17:13:52 +0800

    feat: commands

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -2,20 +2,29 @@
  * @Author: Yorn Qiu
  * @Date: 2023-02-10 15:00:57
  * @LastEditors: Yorn Qiu
- * @LastEditTime: 2023-02-16 14:11:13
+ * @LastEditTime: 2023-02-20 14:57:15
  * @Description: file content
  * @FilePath: /power-copy/src-tauri/src/commands.rs
  */
 
-use crate::database::{Record,DB};
+use crate::database::{Record, DB};
 
 #[tauri::command]
 pub fn get_records() -> Vec<Record> {
-  DB::new().find_all().unwrap()
+    DB::new().find_all().unwrap()
 }
 
-pub fn insert_record(id: String) {}
+#[tauri::command]
+pub fn delete_record(id: u32) {
+    DB::new().delete_by_id(id).unwrap()
+}
 
-pub fn delete_record() {}
+#[tauri::command]
+pub fn delete_records(ids: Vec<u32>) {
+    DB::new().delete_many_by_ids(ids).unwrap()
+}
 
-pub fn clear_record() {}
+#[tauri::command]
+pub fn clear_record() {
+    DB::new().delete_all().unwrap()
+}
